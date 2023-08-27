f02caa812cf986571e23cce76d63d352d7413905
Rankshow <uwaomaobinna20@gmail.com>
2023-08-27 17:20:52 +0100

    added pingIPV4 and pingIPV6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿// ! clasess in C# 
+﻿
 using Classes;
 
-Dog turtle = new Dog();
-turtle.DogBarking();
 
-IDogWalker turtle2 = new Dog();
-turtle.WalkerToThePark();
-turtle.WalkerToTheCity();
-turtle.WalkerToTheFoodBowl();
+
+PingIPV6 pingIPV6 = new PingIPV6();
+Console.WriteLine(pingIPV6.Init());
+Console.WriteLine(pingIPV6.SendPing());
+
+PingIPV4 pingIPV4 = new PingIPV4();
+Console.WriteLine(pingIPV4.Init());
+Console.WriteLine(pingIPV4.SendPing());
